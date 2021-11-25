011b1e68d3c1e9b3313ebad28aca74049f0e0bd8
Kev <kangjun1225@qq.com>
2021-11-25 18:09:19 +0800

    完成json管道

--- mySpider/pipelines.py
+++ mySpider/pipelines.py
@@ -7,9 +7,18 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
-#管道，储存数据
+import json
+
+#管道，储存数据。可以创建多个类，用来做不同数据格式的处理，此为json
 class MyspiderPipeline:
+    #初始化函数，仅在开始运行一次
+    def __init__(self):
+        self.f = open('ITcast_pipeline.json','wb')
 
     def process_item(self, item, spider):
-        #todo
+        content = json.dumps(dict(item), ensure_ascii = False) + '\n'
+        self.f.write(content.encode('utf-8'))
         return item
+
+    def close_spider(self, spider):
+        self.f.close()
